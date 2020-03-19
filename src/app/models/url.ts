9d84984ab80e7b1787b91712c4be56f9518a9ff5
NGAHANE STEVE WILLIAM <ngahanestevewilliam98@gmail.com>
2020-03-19 13:47:47 +0100

    d

--- src/app/models/url.ts
+++
@@ -1,7 +0,0 @@
-export interface Admin {
-    id: number;
-    ip: string;
-    unique: string;
-    redirectUrl: string;
-    createAt: string;
-}
--- src/assets/redirect.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  push: function (route) {
-      let url = "http://localhost:4200"+route;
-      open(url, '_self')
-  }
-};
